b93b8c0fe3e86f6f48fe94727c5b1aeef6a943ce
alexmactavish005-dot <alexmactavish005@gmail.com>
2026-09-14 22:54:08 -0300

    Refactor MainActivity to use Activity class
    Refactor MainActivity to extend Activity instead of ComponentActivity and remove Compose UI setup.

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,46 +1,10 @@
 package com.rutta.app
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 
-class MainActivity : ComponentActivity() {
-
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            RuttaTestScreen()
-        }
-    }
-}
-
-@Composable
-fun RuttaTestScreen() {
-    MaterialTheme {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "RUTTA",
-                fontSize = 32.sp
-            )
-
-            Text(
-                text = "La aplicación inició correctamente",
-                fontSize = 16.sp
-            )
-        }
     }
 }
